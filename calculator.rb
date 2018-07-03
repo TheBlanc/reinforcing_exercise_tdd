86f63d68140017eb32462fe26b6b0a0be6e2e13f
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-03 14:15:57 -0400

    Write add, suntract and sum method based on tests

---
+++ calculator.rb
@@ -0,0 +1,15 @@
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def sum(array)
+  sum = 0
+  array.each do |a|
+    sum += a
+  end
+  return sum
+end
